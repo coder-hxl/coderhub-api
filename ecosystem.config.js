4394ec4ba150d9e7fe6fef49e720e0e977d9bd11
coderhxl <coderhxl@outlook.com>
2022-07-15 13:40:10 +0800

    守护进程

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+const { APP_PORT } = process.env
+
 module.exports = {
   apps: [
     {
-      name: 'coderhub-API-8888',
+      name: `coderhub-API-${APP_PORT}`,
       interpreter: 'node',
       interpreter_args: '-r esbuild-register',
       script: 'src/main.ts',
